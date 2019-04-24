d562ec5135ca29c8c930d4d37e5f45a58cbc2d40
Ricky Singh <oldnewyorker@Rickys-Mac-mini.local>
2019-04-24 12:47:45 -0400

    group 7 project 2

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 9010;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({defaultLayout:"main"}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/loaf_controllers.js");
+
+app.use(routes);
+
+app.use(function(req,res, next){
+    console.log('Time',Date.now());
+    next();
+});
+
+// app.get("/", )
+// app.use(routes);
+
+app.get('/', function (req, res) {
+    res.send('')
+  })
+
+
+app.listen(PORT, function(){
+    console.log("Server listening on http://localhost" + PORT);
+});
+
